745899f93405c05357b7fa0b29a39e934743d7f3
omar gentil manjarres forero <kofperfect922@hotmail.com>
2016-09-12 21:18:38 -0500

    mas cambios

---
+++ GestionHojasDeVida/GestionHojasDeVida/Controllers/EstudiosController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionHojasDeVida.Controllers
+{
+    public class EstudiosController : Controller
+    {
+        // GET: Estudios
+        public ActionResult Estudio()
+        {
+            return View();
+        }
+
+        // GET: Estudios/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Estudios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Estudios/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Estudios/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Estudios/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Estudios/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Estudios/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GestionHojasDeVida/GestionHojasDeVida/Models/EstudiosDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionHojasDeVida.Models
+{
+    public class EstudiosDto
+    {
+        public string institucionEducativa { get; set; }
+        public string Titulo_obtenido { get; set; }
+        public string nvlDeestudio { get; set; }
+        public DateTime fechafinalizacion { get; set; }
+        public string  Estado { get; set; }
+
+
+    }
+}
