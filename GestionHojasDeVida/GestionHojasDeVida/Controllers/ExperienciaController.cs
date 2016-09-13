198b126f1270ff0ec967eb8de0ec63e08194565e
omar gentil manjarres forero <kofperfect922@hotmail.com>
2016-09-12 22:40:20 -0500

    listo para enviar los datos de experiencia y estudios cristian haga algo a ver

---
+++ GestionHojasDeVida/GestionHojasDeVida/Controllers/ExperienciaController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionHojasDeVida.Controllers
+{
+    public class ExperienciaController : Controller
+    {
+        // GET: Experiencia
+        public ActionResult Experiencia()
+        {
+            return View();
+        }
+
+        // GET: Experiencia/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Experiencia/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Experiencia/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+
+            // aca se llama un objeto de tipo Experiencia Dao que es el que genera las consultas e inserta la experiencia
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Experiencia/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Experiencia/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Experiencia/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Experiencia/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GestionHojasDeVida/GestionHojasDeVida/Models/ExperienciaDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestionHojasDeVida.Models
+{
+    public class ExperienciaDto
+    {
+        public string Empresa { get; set; }
+        public string Cargo { get; set; }
+        [Display(Name = "Fecha Inicio", Description = "Describa sus funciones y logros")]
+        public DateTime fechaInicio { get; set; }
+        [Display(Name = "Fecha Fin", Description = "Describa sus funciones y logros")]
+        public DateTime FechaFin { get; set; }
+        [Display(Name = "Funciones y Logros", Description = "Describa sus funciones y logros")]
+        public string fun_logros { get; set; }
+    }
+}
