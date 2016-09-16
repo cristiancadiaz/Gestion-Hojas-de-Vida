872b64cdb9efeb93bf568d976124394d7ff4f4d4
Bision-PC <Bision-PC@LENOVO-PC1>
2016-09-16 15:44:26 -0500

    inactivarDao

--- GestionHojasDeVida/GestionHojasDeVida/Models/InactivarDao.cs
+++ GestionHojasDeVida/GestionHojasDeVida/Models/InactivarDao.cs
@@ -7,6 +7,26 @@ namespace GestionHojasDeVida.Models
 {
     public class InactivarDao
     {
+        public int Autenticar(string username, string password)
 
+        {
+            SqlParameter Identificacion = new SqlParameter();
+            Identificaciono.ParameterName = "@Identificacion";
+            Identificacion.SqlDbType = SqlDbType.Int;
+            Identificacion.Value = rudto.identificacion;
+
+            SqlParameter Estado = new SqlParameter();
+            Estado.ParameterName = "@Estado";
+            Estado.SqlDbType = SqlDbType.VarChar;
+            Estado.Value = rudto.Estado;
+
+            SqlParameter Motivo = new SqlParameter();
+            Motivo.ParameterName = "@Motivo";
+            Motivo.SqlDbType = SqlDbType.VarChar;
+            Motivo.Value = rudto.Motivo;
+        }
+        public void Inactivar()
+        { }
     }
+    
 }
