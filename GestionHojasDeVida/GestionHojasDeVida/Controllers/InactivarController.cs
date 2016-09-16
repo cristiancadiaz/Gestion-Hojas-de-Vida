9e9f03157635d5b7bbc1f735a611ba5307b7e835
Yeimmy <Yeimmy@YEIMMY-PC>
2016-09-16 06:08:10 -0500

    Creacion controlador, dao, dto Inactivar

---
+++ GestionHojasDeVida/GestionHojasDeVida/Controllers/InactivarController.cs
@@ -0,0 +1,90 @@
+﻿using GestionHojasDeVida.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionHojasDeVida.Controllers
+{
+    public class InactivarController : Controller
+    {
+        // GET: Inactivar
+        public ActionResult Inactivar()
+        {
+            return View();
+        }
+
+        // GET: Inactivar/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Inactivar/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inactivar/Create
+        [HttpPost]
+        public ActionResult Create(InactivarDto inac)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Inactivar/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Inactivar/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Inactivar/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Inactivar/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GestionHojasDeVida/GestionHojasDeVida/Models/InactivarDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionHojasDeVida.Models
+{
+    public class InactivarDto
+    {
+        public int Identificacion { get; set; }
+        public int Estado { get; set; }
+        public string  Motivo { get; set; }
+    }
+
+
+}
