bc1019804ad30f11a1179674e41459af1319c67d
bryansthr <bryansthr@gmail.com>
2016-09-16 21:31:25 -0500

    Metodo Inactivar

--- GestionHojasDeVida/GestionHojasDeVida/Models/InactivarDao.cs
+++ GestionHojasDeVida/GestionHojasDeVida/Models/InactivarDao.cs
@@ -39,6 +39,7 @@ namespace GestionHojasDeVida.Models
 
             SqlDataAdapter adaptador = new SqlDataAdapter(comando);
 
+
             conexion.Open();
             // comando.ExecuteScalar();
 
